a5add22dc03af778f1e44f77a3b4f17e34801764
MauriNestor <mauric845apaza@gmail.com>
2025-01-25 23:28:51 -0400

    add some dependencies

---
+++ app/assets/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    alias(libs.plugins.jetbrains.kotlin.android)
+}
+android {
+    namespace = "org.scesi.assets"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 24
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
+
+dependencies {
+    implementation(project(":app"))
+    implementation(libs.androidx.core.ktx)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
