d93b03d38dc1eb7973e0458d481e85c89f773a36
MauriNestor <mauric845apaza@gmail.com>
2025-02-06 23:54:15 -0400

    add scafoold in poseScreen composable

--- app/src/main/java/com/pose_estimation/ui/PoseApp.kt
+++ app/src/main/java/com/pose_estimation/ui/PoseApp.kt
@@ -1,10 +1,20 @@
 package com.pose_estimation.ui
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.NavHostController
 import com.pose_estimation.detector.PoseDetector
 import com.pose_estimation.navigation.AppNavigation
+import com.pose_estimation.ui.screens.BottomNavigationBar
 import com.pose_estimation.ui.theme.Pose_EstimationTheme
 
 @Composable
@@ -13,7 +23,7 @@ fun PoseApp() {
     val poseDetector = PoseDetector(context)
 
     val appState = rememberPoseAppState()
-    PoseScreen {
+    PoseScreen(navController = appState.navController) {
         AppNavigation(
             navController = appState.navController,
             poseDetector = poseDetector
@@ -22,10 +32,14 @@ fun PoseApp() {
 }
 
 @Composable
-fun PoseScreen(content: @Composable () -> Unit) {
+fun PoseScreen(navController: NavHostController, content: @Composable () -> Unit) {
     Pose_EstimationTheme {
-        Surface {
-            content()
+        Scaffold(
+            bottomBar = { BottomNavigationBar(navController) }
+        ) { innerPadding ->
+            Surface(modifier = Modifier.padding(innerPadding)) {
+                content()
+            }
         }
     }
 }
