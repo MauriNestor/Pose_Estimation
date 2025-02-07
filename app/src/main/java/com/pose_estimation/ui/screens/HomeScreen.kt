0d0e88746a7897d3079e6a4ee91333b36e0dbb51
MauriNestor <mauric845apaza@gmail.com>
2025-02-07 19:33:04 -0400

    refactor homscreen

--- app/src/main/java/com/pose_estimation/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/pose_estimation/ui/screens/HomeScreen.kt
@@ -6,12 +6,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.pose_estimation.navigation.AppScreens
 
 @Composable
 fun HomeScreen(navController: NavHostController) {
+    HomeScreenContent(onNavigate = { navController.navigate(AppScreens.ImageSelectorScreen.route) })
+}
+
+@Composable
+fun HomeScreenContent(onNavigate: () -> Unit) {
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
@@ -21,8 +27,14 @@ fun HomeScreen(navController: NavHostController) {
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        Button(onClick = { navController.navigate(AppScreens.ImageSelectorScreen.route) }) {
+        Button(onClick = onNavigate) {
             Text("Ir a Selección de Imagen")
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview() {
+    HomeScreenContent(onNavigate = {})
 }
