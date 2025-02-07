460b5ed4b84fc6479a6cb148946b245de8918835
MauriNestor <mauric845apaza@gmail.com>
2025-02-06 23:52:57 -0400

    pdf screen init

---
+++ app/src/main/java/com/pose_estimation/ui/screens/PDFScreen.kt
@@ -0,0 +1,22 @@
+package com.pose_estimation.ui.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+
+@Composable
+fun PDFScreen(navController: NavHostController) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "Gestor de PDFs")
+    }
+}
