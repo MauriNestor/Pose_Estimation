f90c1a1f132ed395f018daade0c27c4c0e44bf13
MauriNestor <mauric845apaza@gmail.com>
2025-02-08 19:43:40 -0400

    add initial screen of sttings and useprofile

---
+++ app/src/main/java/com/pose_estimation/ui/screens/SettingsScreen.kt.kt
@@ -0,0 +1,35 @@
+package com.pose_estimation.ui.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+
+@Composable
+fun SettingsScreen(navController: NavHostController) {
+    AppScaffold {
+        SettingsScreenContent()
+    }
+}
+
+@Composable
+fun SettingsScreenContent() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Configuraciones")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SettingsScreenPreview() {
+    SettingsScreenContent()
+}
---
+++ app/src/main/java/com/pose_estimation/ui/screens/UserProfileScreen.kt
@@ -0,0 +1,34 @@
+package com.pose_estimation.ui.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun UserProfileScreen() {
+    AppScaffold {
+        UserProfileScreenContent()
+    }
+}
+
+@Composable
+fun UserProfileScreenContent() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Perfil de Usuario")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun UserProfileScreenPreview() {
+    UserProfileScreenContent()
+}
