04a22189b55fbd4322e1acc3af94b6a34e990fb2
MauriNestor <mauric845apaza@gmail.com>
2025-02-10 23:56:14 -0400

    update iu homescreen

--- app/src/main/java/com/pose_estimation/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/pose_estimation/ui/screens/HomeScreen.kt
@@ -1,14 +1,29 @@
 package com.pose_estimation.ui.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import coil.compose.AsyncImage
 import com.pose_estimation.navigation.AppScreens
 
 @Composable
@@ -23,16 +38,87 @@ fun HomeScreen(navController: NavHostController) {
 @Composable
 fun HomeScreenContent(onNavigate: () -> Unit) {
     Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+            .padding(top = 56.dp),
+        verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Bienvenido a Pose Estimation")
+//        Text(
+//            text = "Bienvenido a Pose Estimation",
+//            style = MaterialTheme.typography.bodyLarge,
+//        )
+        FeatureCard(
+            title = "Análisis rápido",
+            imageUrl = "android.resource://com.pose_estimation/drawable/postura",
+            backgroundColor = Color.White,
+            onClick = { onNavigate() }
+        )
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        Button(onNavigate) {
-            Text("Ir a ImageSelectorScreen")
+        FeatureCard(
+            title = "Ejercicios Recomendados",
+            imageUrl = "android.resource://com.pose_estimation/drawable/postura",
+            backgroundColor = Color.White,
+            onClick = { /* Futuro: navController.navigate(AppScreens.ExercisesScreen) */ }
+        )
+
+    }
+}
+
+@Composable
+fun FeatureCard(title: String, imageUrl: String, backgroundColor: Color, onClick: () -> Unit) {
+    Card(
+        shape = RoundedCornerShape(20.dp),
+        colors = CardDefaults.cardColors(containerColor = backgroundColor),
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(100.dp)
+            .shadow(8.dp, RoundedCornerShape(20.dp))
+            .clickable { onClick() }
+            .padding(start = 8.dp, end = 8.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(6.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            AsyncImage(
+                model = imageUrl,
+                contentDescription = "Imagen de $title",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(80.dp)
+                    .background(Color.LightGray, shape = RoundedCornerShape(12.dp))
+            )
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            // Título
+            Text(
+                text = title,
+                style = MaterialTheme.typography.titleLarge,
+                color = Color.Black,
+                modifier = Modifier.weight(1f)
+                    .padding(bottom = 25.dp)
+            // Expande el texto para ocupar el espacio disponible
+            )
+
+            // Botón con ícono de flecha
+            Box(
+                modifier = Modifier
+                    .size(36.dp)
+                    .background(Color(0xFF1E1E2D), shape = CircleShape)
+                    .clickable { onClick() },
+                contentAlignment = Alignment.Center
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.ArrowForward,
+                    contentDescription = "Ir a $title",
+                    tint = Color.White
+                )
+            }
         }
     }
 }
