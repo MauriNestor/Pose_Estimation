9ab01c0ec34166bb4008577ab42bd62620568697
MauriNestor <mauric845apaza@gmail.com>
2025-02-17 23:30:51 -0400

    refactor home screen

--- app/src/main/java/com/pose_estimation/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/pose_estimation/ui/screens/HomeScreen.kt
@@ -2,12 +2,21 @@ package com.pose_estimation.ui.screens
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowForward
-import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
@@ -19,7 +28,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
@@ -43,10 +51,6 @@ fun HomeScreenContent(onNavigate: () -> Unit) {
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-//        Text(
-//            text = "Bienvenido a Pose Estimation",
-//            style = MaterialTheme.typography.bodyLarge,
-//        )
         FeatureCard(
             title = "Análisis rápido",
             imageUrl = "android.resource://com.pose_estimation/drawable/postura",
@@ -65,7 +69,6 @@ fun HomeScreenContent(onNavigate: () -> Unit) {
 
     }
 }
-
 @Composable
 fun FeatureCard(title: String, imageUrl: String, backgroundColor: Color, onClick: () -> Unit) {
     Card(
@@ -102,10 +105,8 @@ fun FeatureCard(title: String, imageUrl: String, backgroundColor: Color, onClick
                 color = Color.Black,
                 modifier = Modifier.weight(1f)
                     .padding(bottom = 25.dp)
-            // Expande el texto para ocupar el espacio disponible
             )
 
-            // Botón con ícono de flecha
             Box(
                 modifier = Modifier
                     .size(36.dp)
@@ -122,7 +123,6 @@ fun FeatureCard(title: String, imageUrl: String, backgroundColor: Color, onClick
         }
     }
 }
-
 @Preview(showBackground = true)
 @Composable
 fun HomeScreenPreview() {
