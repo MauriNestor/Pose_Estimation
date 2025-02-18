8278ea45d490b489d170b2becd267201535770fe
MauriNestor <mauric845apaza@gmail.com>
2025-02-17 23:29:54 -0400

    refactor: imports unused in main activity

--- app/src/main/java/com/pose_estimation/MainActivity.kt
+++ app/src/main/java/com/pose_estimation/MainActivity.kt
@@ -1,21 +1,9 @@
 package com.pose_estimation
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.rememberNavController
-import androidx.navigation.compose.composable
-import com.pose_estimation.detector.PoseDetector
-import com.pose_estimation.navigation.AppScreens
 import com.pose_estimation.ui.PoseApp
-import com.pose_estimation.ui.screens.HomeScreen
-import com.pose_estimation.ui.screens.ImageSelectorScreen
-import com.pose_estimation.ui.theme.Pose_EstimationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
